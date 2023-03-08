090658e3875af4f9a10324103198fac88f7dad48
Samet DOGAN <sametdogan23@protonmail.ch>
2023-03-08 22:59:15 +0300

    entities and context class created

---
+++ DataAccess/Concrete/Context.cs
@@ -0,0 +1,30 @@
+﻿using Entity.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete
+{
+    public class Context : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=DESKTOP-LVPTDQG\\SQLEXPRESS;database=TraversalDB;integrated security=true"); ;
+        }
+
+        public DbSet<About> Abouts { get; set; }
+        public DbSet<About2> Abouts2 { get; set; }
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<Destination> Destinations { get; set; }
+        public DbSet<Guide> Guides { get; set; }
+        public DbSet<MainFeature> MainFeatures { get; set; }
+        public DbSet<Newsletter> Newsletters { get; set; }
+        public DbSet<SideFeature> SideFeatures { get; set; }
+        public DbSet<SubAbout> SubAbouts { get; set; }
+        public DbSet<Testimonial> Testimonials { get; set; }
+
+    }
+}
